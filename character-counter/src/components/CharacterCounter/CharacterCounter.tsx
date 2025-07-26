413fc97025078e70b903b81651ed0d3fd586bdcd
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-26 12:26:33 -0400

    Completed impleentation of CharacterCounter.

--- character-counter/src/components/CharacterCounter/CharacterCounter.tsx
+++ character-counter/src/components/CharacterCounter/CharacterCounter.tsx
@@ -1,21 +1,25 @@
 import { type ReactNode } from "react"
 import { type CharacterCounterProps } from "../../types"
 
+import { numberToTimeString } from "../../utils"
+
 export default function CharacterCounter({
   minWords,
   maxWords,
   targetReadingTime,
 }: CharacterCounterProps): ReactNode {
-  return (
-    <div>
-      <h2>CharacterCounter</h2>
-      {(minWords || maxWords || targetReadingTime) && (
-        <ul>
-          {minWords && <li>minWords: {minWords}</li>}
-          {maxWords && <li>maxWords: {maxWords}</li>}
-          {targetReadingTime && <li>targetReadingTime: {targetReadingTime}</li>}
-        </ul>
-      )}
-    </div>
-  )
+  function makeCharacterCounterString(): string {
+    const displayStringItems: string[] = []
+
+    if (minWords !== undefined) displayStringItems.push(`Min: ${minWords}`)
+    if (maxWords !== undefined) displayStringItems.push(`Max: ${maxWords}`)
+    if (targetReadingTime !== undefined)
+      displayStringItems.push(
+        `Target Reading Time: ${numberToTimeString(targetReadingTime)}`
+      )
+
+    return displayStringItems.join(" | ")
+  }
+
+  return <div className="text-center">{makeCharacterCounterString()}</div>
 }
